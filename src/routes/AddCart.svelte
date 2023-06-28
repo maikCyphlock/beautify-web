6a6349829302d152e95fae95a6c7951a9196f582
maikCyphlock <maikolaguilar656@gmail.com>
2023-06-28 17:26:58 -0400

    fixing bugs on buttons

--- src/routes/AddCart.svelte
+++ src/routes/AddCart.svelte
@@ -32,6 +32,7 @@
 					<div class="flex items-center">
 						<span class="text-gray-600">${item.price}</span>
 						<button
+							style="pointer-events: all;"
 							class="ml-2 text-red-600 hover:text-red-800"
 							on:click={() => removeItem(item.id)}>Quitar</button
 						>
@@ -47,6 +48,7 @@
 				>
 			</div>
 			<button
+				style="pointer-events: all;"
 				class="mt-3 w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
 				on:click={toggleCart}>Cerrar carrito</button
 			>
@@ -54,8 +56,23 @@
 	</div>
 </div>
 <button
-	class="fixed bottom-0 right-0 mb-4 mr-4 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full z-20"
+	class="fixed bottom-0 right-0 mb-4 mr-4 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-sm z-20"
 	on:click={toggleCart}
 >
-	<span class="text-xl">&#128722;</span>
+	<span class="text-xl font-bold"
+		><svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+			class="w-6 h-6"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+			/>
+		</svg>
+	</span>
 </button>
